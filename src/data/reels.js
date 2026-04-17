582a6d57674fe30da17437b28d19cc00e9b81114
Sujoyi0001 <sujoygarai89@gmail.com>
2026-04-17 20:02:32 +0530

    Add reels data module with multimedia content and links
    - Created a new file `reels.js` in the data directory.
    - Imported images for each reel from the assets.
    - Defined an array of reel objects, each containing an id, title, description, image, and URL.

---
+++ src/data/reels.js
@@ -0,0 +1,53 @@
+import img1 from '../assets/img/IMAX.png';
+import img2 from '../assets/img/THE DEFOCUSED.png';
+import img3 from '../assets/img/EP03 KASHI.png';
+import img4 from '../assets/img/Ghar.png';
+import img5 from '../assets/img/Let it Rain.png';
+import img6 from '../assets/img/noboborsho.jpeg';
+
+export const REELS = [
+  {
+    id: 1,
+    title: "Kashi,",
+    description: " But from a  Closer Perspective.",
+    image: img3,
+    url: "https://www.instagram.com/reel/DTscgK6D04r/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D"
+  },
+  {
+    id: 2,
+    title: "Experience your roots",
+    description: "Ghar 💛✨",
+    image: img4,
+    url: "https://www.instagram.com/reel/DWd-vnQB3hG/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+  },
+  
+  {
+    id: 3,
+    title: "IMAX Format",
+    description: "Experience the Extra.",
+    image: img1,
+    url: "https://www.instagram.com/reel/DQIqEQCiS0n/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D"
+  },
+  {
+    id: 4,
+    title: "Latest Work",
+    description: "Nobo borsho",
+    image: img6,
+    url: "https://www.instagram.com/reel/DXG3qnrCTb2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+  },
+  
+  {
+    id: 5,
+    title: "The Starting of My Classics",
+    description: "Let it Rain.",
+    image: img5,
+    url: "https://www.instagram.com/reel/CuEraa4t6sN/?igsh=M251eXJyMjBjOGVp"
+  },
+  {
+    id: 6,
+    title: "Experience Interstellar",
+    description: "But on a New Perspective.",
+    image: img2,
+    url: "https://www.instagram.com/reel/C8gTAzuSbee/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D"
+  },
+];
--- src/sections/Reel.jsx
+++ src/sections/Reel.jsx
@@ -1,49 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { motion } from "framer-motion";
-import img1 from '../assets/img/IMAX.png'
-import img2 from '../assets/img/THE DEFOCUSED.png'
-import img3 from '../assets/img/EP03 KASHI.png'
-import img4 from '../assets/img/Ghar.png'
-import img5 from '../assets/img/Let it Rain.png'
-
-// 1. Added dummy Instagram URLs to each reel object
-const REELS = [
-  { 
-    id: 1, 
-    title: "Kashi,", 
-    description: " But from a  Closer Perspective.", 
-    image: img3,
-    url: "https://www.instagram.com/reel/DTscgK6D04r/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D" 
-  },
-  { 
-    id: 2, 
-    title: "Experience Interstellar", 
-    description: "But on a New Perspective.", 
-    image: img2,
-    url: "https://www.instagram.com/reel/C8gTAzuSbee/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D" 
-  },
-  { 
-    id: 3, 
-    title: "IMAX Format", 
-    description: "Experience the Extra.", 
-    image: img1,
-    url: "https://www.instagram.com/reel/DQIqEQCiS0n/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA%3D%3D" 
-  },
-  { 
-    id: 4, 
-    title: "Latest Work", 
-    description: "Ghar 💛✨", 
-    image: img4,
-    url: "https://www.instagram.com/reel/DWd-vnQB3hG/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" 
-  },
-  { 
-    id: 5, 
-    title: "The Starting of My Classics", 
-    description: "Let it Rain.", 
-    image: img5,
-    url: "https://www.instagram.com/reel/CuEraa4t6sN/?igsh=M251eXJyMjBjOGVp" 
-  },
-];
+import { REELS } from "../data/reels";
 
 export default function CenteredReel() {
   const scrollRef = useRef(null);
